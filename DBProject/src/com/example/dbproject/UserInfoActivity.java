d86931d91f2179142a851f233949916e8e41f427
karan <karan@Karan-PC>
2014-11-14 18:37:31 +0800

    commit

---
+++ DBProject/src/com/example/dbproject/UserInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.dbproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserInfoActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.user_info_view);
+	    // TODO Auto-generated method stub
+	}
+
+}
